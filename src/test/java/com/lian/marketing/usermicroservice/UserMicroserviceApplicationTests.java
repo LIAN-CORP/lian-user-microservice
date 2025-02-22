0f2534426d9a589a507a1b73fc969e535b04b6ad
3lPr1mo <erickenriquechaparro@gmail.com>
2025-02-22 14:24:47 -0500

    Project init

---
+++ src/test/java/com/lian/marketing/usermicroservice/UserMicroserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.usermicroservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserMicroserviceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
