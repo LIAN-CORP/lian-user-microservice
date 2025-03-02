fa433aaedd04adf0546194acc6279814f416a8b6
3lPr1mo <erickenriquechaparro@gmail.com>
2025-03-01 19:22:01 -0500

    feat(app): add logic to handle create user

---
+++ src/main/java/com/lian/marketing/usermicroservice/application/constants/RequestConstants.java
@@ -0,0 +1,13 @@
+package com.lian.marketing.usermicroservice.application.constants;
+
+public class RequestConstants {
+    private RequestConstants() {}
+
+    public static final String FIRST_NAME_MUST_BE_NOT_NULL = "First name must not be null";
+    public static final String LAST_NAME_MUST_BE_NOT_NULL = "Last name must not be null";
+    public static final String EMAIL_MUST_BE_NOT_NULL = "Email must not be null";
+    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$";
+    public static final String EMAIL_IS_NOT_VALID = "Email is not valid";
+    public static final String PASSWORD_MUST_BE_NOT_NULL = "Password must not be null";
+    public static final String BIRTHDAY_MUST_BE_NOT_NULL = "Birthday must not be null";
+}
