84fd4dbbeb011b037d9b09dfddd659dbbdd4f290
3lPr1mo <erickenriquechaparro@gmail.com>
2025-09-12 17:44:04 -0500

    refactor(all): remove email verification-related constants, models, and database entities

---
+++ src/main/resources/db/migration/V1_1__CREATE_USER_TABLE.sql
@@ -0,0 +1,11 @@
+CREATE TABLE IF NOT EXISTS _user (
+                              id uuid DEFAULT uuid_generate_v4() NOT NULL,
+                              first_name varchar(255) NOT NULL,
+                              last_name varchar(255) NOT NULL,
+                              email varchar(320) NOT NULL,
+                              "password" varchar(255) NOT NULL,
+                              birthday date NOT NULL,
+                              CONSTRAINT user_email_check CHECK (((email)::text ~* '^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,}$'::text)),
+	CONSTRAINT user_email_key UNIQUE (email),
+	CONSTRAINT user_pkey PRIMARY KEY (id)
+);
