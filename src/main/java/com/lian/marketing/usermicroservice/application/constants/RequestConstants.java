6758b840da6ffe6d2c78b366230fa0e0ac78b519
3lPr1mo <erickenriquechaparro@gmail.com>
2025-09-12 17:46:00 -0500

    refactor(app): remove email verification request models

--- src/main/java/com/lian/marketing/usermicroservice/application/constants/RequestConstants.java
+++ src/main/java/com/lian/marketing/usermicroservice/application/constants/RequestConstants.java
@@ -6,11 +6,7 @@ public class RequestConstants {
     public static final String FIRST_NAME_MUST_BE_NOT_NULL = "First name must not be null";
     public static final String LAST_NAME_MUST_BE_NOT_NULL = "Last name must not be null";
     public static final String EMAIL_MUST_BE_NOT_NULL = "Email must not be null";
-    public static final String EMAIL_REGEX = "^[A-Za-z0-9._%+-]+@[A-Za-z0-9.-]+\\\\.[A-Za-z]{2,}$";
     public static final String EMAIL_IS_NOT_VALID = "Email is not valid";
     public static final String PASSWORD_MUST_BE_NOT_NULL = "Password must not be null";
     public static final String BIRTHDAY_MUST_BE_NOT_NULL = "Birthday must not be null";
-    public static final String VERIFICATION_CODE_MUST_BE_NOT_EMPTY = "Verification code must not be empty";
-    public static final String VERIFICATION_CODE_IS_NOT_VALID = "Verification code is not valid";
-    public static final String CODE_REGEX = "^[0-9]*$";
 }
